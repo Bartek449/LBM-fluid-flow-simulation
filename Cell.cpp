f6e1de5e9570551701784b11bb2826e77280fa93
Bartek449 <bbszwarga@gmail.com>
2024-12-27 12:15:43 +0100

    początek zmian na D2Q9

--- Cell.cpp
+++ Cell.cpp
@@ -1,12 +1,14 @@
 #include"Cell.h"
 
-Cell::Cell() : fun_in(EMPTY), fun_ex(EMPTY), fun_eq(EMPTY), density(0), weight(0.25) {}
+Cell::Cell() : fun_in(EMPTY), fun_ex(EMPTY), fun_eq(EMPTY), density(0) {}
 
-Cell::Cell(array<double, 4> i) : fun_in(i), fun_ex(EMPTY), fun_eq(EMPTY), density(0), weight(0.25) {
+Cell::Cell(array<double, 9> i) : fun_in(i), fun_ex(EMPTY), fun_eq(EMPTY), density(0){
     calculate_density();
+    calculate_velocity();
 }
 
-void Cell::set_fun(FunType f, array<double,4> i) {
+
+void Cell::set_fun(FunType f, array<double,9> i) {
     if(f == FUN_IN) fun_in = i;
     else if (f == FUN_EX) fun_ex = i;
     else if (f == FUN_EQ) fun_eq = i;
@@ -18,10 +20,29 @@ void Cell::set_direct_fun(FunType f, int index, double num) {
     else if (f == FUN_EQ) fun_eq[index] = num;
 }
 
-const array<double,4>& Cell::get_fun(FunType f) const { 
+void Cell::set_velocity(array<double, 2> v) {
+    velocity = v;}
+
+void Cell::set_direct_velocity(int index, double num) {
+    velocity[index] = num;
+}
+
+
+const array<double,9>& Cell::get_fun(FunType f) const { 
     if (f == FUN_IN) return fun_in;
     else if (f == FUN_EX) return fun_ex;
     else if (f == FUN_EQ) return fun_eq;;
+}
+
+const array<double, 2>& Cell::get_velocity() const { return velocity; }
+
+double Cell::get_density() { return density; }
+
+
+void::Cell::calculate_velocity() {
+    calculate_density();
+    velocity[0] = (fun_in[1] + fun_in[5] + fun_in[8] - fun_in[2] - fun_in[6] - fun_in[7])/density;
+    velocity[1] = (fun_in[3] + fun_in[5] + fun_in[6] - fun_in[4] - fun_in[7] - fun_in[8]) / density;
 }
 
 void Cell::calculate_density()
@@ -34,19 +55,18 @@ void Cell::calculate_density()
 
 }
 
-double Cell::get_density() { return density; }
-
-
 void Cell::calculate_fun_eq() { 
-    for (int i = 0; i < 4; ++i) {
-        fun_eq[i] = density * weight;
+    
+    for (int i = 0; i < 9; ++i) {
+        double sc = (CI[i][0] * velocity[0]) + (CI[i][0] * velocity[1]);
+        fun_eq[i] = (density * WEIGHT[i])*(1+ 3*sc + 4.5 * pow(sc,2) - 1.5 * (pow(velocity[0], 2) + pow(velocity[1], 2)));
     } 
 }
 
 void Cell::calculate_fun_ex() 
 { 
     calculate_fun_eq();
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 9; ++i) {
         fun_ex[i] = fun_in[i] + (1.0 / RELAXATION_TIME) * (fun_eq[i] - fun_in[i]);
      }
 }
--- Cell.h
+++ Cell.h
@@ -5,31 +5,37 @@
 
 using namespace std;
 
-constexpr array<double, 4> EMPTY = { 0.0,0.0,0.0,0.0 };
-constexpr array<double, 4> STARTING_STATE = { 0.25,0.25,0.25,0.25 };
-constexpr array<double, 4> WALL = { -1.0,-1.0,-1.0,-1.0 };
+constexpr array<double, 9> EMPTY = { 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 };
+constexpr array<double, 9> STARTING_STATE = { 0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25 };
+constexpr array<double, 9> WALL = { -1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0 };
+constexpr array<double, 9> WEIGHT = { 4/9, 1/9, 1/9, 1/9, 1/ 9, 1/36, 1/36, 1/36, 1/36 };
+constexpr array<array<double, 2>, 9> CI = { { {{0, 0}}, {{1, 0}}, {{-1, 0}}, {{0, 1}},{{0, -1}}, {{1, 1}}, {{-1, 1}}, {{-1, -1}}, {{1, -1}}} };
 constexpr double RELAXATION_TIME = 0.75;
 
 enum FunType{FUN_IN,FUN_EX, FUN_EQ};
 
 class Cell {
 private:
-    array <double, 4> fun_in;
-    array <double, 4> fun_ex;
-    array <double, 4> fun_eq;
+    array <double, 9> fun_in;
+    array <double, 9> fun_ex;
+    array <double, 9> fun_eq;
+    array<double, 2> velocity;
     double density;
-    double weight;
 
 public:
     Cell();
-    Cell(array<double,4>);
+    Cell(array<double,9>);
 
-    void set_fun(FunType, array<double,4>);
+    void set_fun(FunType, array<double,9>);
     void set_direct_fun(FunType, int, double);
+    void set_velocity(array<double,2>);
+    void set_direct_velocity(int, double);
 
-    const array<double,4>& get_fun(FunType) const;
+    const array<double,9>& get_fun(FunType) const;
+    const array<double, 2>& get_velocity() const;
     double get_density();
 
+    void calculate_velocity();
     void calculate_density();
     void calculate_fun_eq();
     void calculate_fun_ex();
